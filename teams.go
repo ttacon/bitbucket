0de2f6fde66a6f6affed91132a66b3999f60e71c
ttacon <ttacon@gmail.com>
2014-07-25 08:10:54 -0400

    Add get team members

--- teams.go
+++ teams.go
@@ -40,3 +40,45 @@ func (t TeamService) GetTeamProfile(team string) (*Team, error) {
 
 	return &data, nil
 }
+
+type TeamMembers struct {
+	PageLen int          `json:"pagelen"`
+	Values  []TeamMember `json:"values"`
+	Page    int          `json:"page"`
+	Size    int          `json:"size"`
+}
+
+// TODO(ttacon): combine with owner/user?
+
+type TeamMember struct {
+	Username    string `json:"username"`
+	Kind        string `json:"kind"`
+	Website     string `json:"website"`
+	DisplayName string `json:"display_name"`
+	Links       Links  `json:"links"`
+	CreatedOn   string `json:"created_on"`
+	Location    string `json:"location"`
+}
+
+func (t TeamService) GetTeamMembers(team string) (*TeamMembers, error) {
+	req, err := http.NewRequest("GET",
+		fmt.Sprintf("%s/teams/%s/members", V2_URL, team),
+		nil)
+	if err != nil {
+		return nil, err
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var data TeamMembers
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		return nil, err
+	}
+
+	return &data, nil
+}
